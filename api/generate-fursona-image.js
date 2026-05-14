602c1be64a94d67461ebe0354004be466d407b26
naeemsan <116320099+naeemsan@users.noreply.github.com>
2026-05-14 05:34:40 +0500

    Update API endpoint for fursona image generation

--- api/generate-fursona-image.js
+++ api/generate-fursona-image.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
     ].join(", ");
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/Lykon/dreamshaper-8",
+      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
       {
         method: "POST",
         headers: {
