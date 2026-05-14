f3c44ff795f24a82145f54b11605f51e5bd471ca
naeemsan <116320099+naeemsan@users.noreply.github.com>
2026-05-14 05:44:10 +0500

    Update generate-fursona-image.js

--- api/generate-fursona-image.js
+++ api/generate-fursona-image.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
     ].join(", ");
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+      "https://api-inference.huggingface.co/models/stabilityai/black-forest-labs/FLUX.1-dev",
       {
         method: "POST",
         headers: {
