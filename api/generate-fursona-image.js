9833077a453973b852442ff90ee6d34820ef2f77
naeemsan <116320099+naeemsan@users.noreply.github.com>
2026-05-14 05:39:33 +0500

    Update API endpoint for image generation

--- api/generate-fursona-image.js
+++ api/generate-fursona-image.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
     ].join(", ");
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
       {
         method: "POST",
         headers: {
