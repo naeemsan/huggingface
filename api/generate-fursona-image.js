5f8c4b690ce7cce32dd3f9a0b6124fd119c39c6d
naeemsan <116320099+naeemsan@users.noreply.github.com>
2026-05-14 05:21:50 +0500

    Add CORS headers to fursona image API handler

--- api/generate-fursona-image.js
+++ api/generate-fursona-image.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "GET") {
     return res.status(200).json({
       ok: true,
